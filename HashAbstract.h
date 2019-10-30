c02d595b62342218f2774571dd8fcf5aa07f823d
hanjun <h1212@g.ucla.edu>
2019-10-30 15:18:43 -0400

    pure reformatting (clang-format)

--- HashAbstract.h
+++ HashAbstract.h
@@ -2,36 +2,31 @@
 #ifndef INC_20160901_MALICIOUSMPC_DOUBLEBUFFER_HASHABSTRACT_HPP
 #define INC_20160901_MALICIOUSMPC_DOUBLEBUFFER_HASHABSTRACT_HPP
 
-#include <openssl/evp.h>
 #include <iostream> //for testing
+#include <openssl/evp.h>
 
 //#define USECXXPROF
 //#include <cxxprof_static/CxxProf.h>
-
 
 using namespace std;
 
 /**
  * Abstract class for defining the interface of hasing: init, update and final
  */
-class HashAbstract
-{
+class HashAbstract {
 public:
-    virtual void hashUpdate(unsigned char *in, int inSizeBytes) = 0;
-    // hashFinal implementation should set initialized to false
-    virtual void hashFinal(unsigned char *out, unsigned int *outSizeBytes) = 0;
-//    virtual void hashReset() = 0;
+  virtual void hashUpdate(unsigned char *in, int inSizeBytes) = 0;
+  // hashFinal implementation should set initialized to false
+  virtual void hashFinal(unsigned char *out, unsigned int *outSizeBytes) = 0;
+  //    virtual void hashReset() = 0;
 
-    void getHashedDataOnce(unsigned char *in, int inSizeBytes, unsigned char *out,
-                           unsigned int *outSizeBytes)
-    {
-        //CXXPROF_ACTIVITY("hash");
+  void getHashedDataOnce(unsigned char *in, int inSizeBytes, unsigned char *out,
+                         unsigned int *outSizeBytes) {
+    // CXXPROF_ACTIVITY("hash");
 
-        hashUpdate(in, inSizeBytes);
-        hashFinal(out, outSizeBytes);
-    }
-
+    hashUpdate(in, inSizeBytes);
+    hashFinal(out, outSizeBytes);
+  }
 };
 
-
-#endif //INC_20160901_MALICIOUSMPC_DOUBLEBUFFER_HASHABSTRACT_HPP
+#endif // INC_20160901_MALICIOUSMPC_DOUBLEBUFFER_HASHABSTRACT_HPP
